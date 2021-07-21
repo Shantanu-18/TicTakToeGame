aa39f1b56c6f3e748f3a22f71d42730c8da01647
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-21 11:31:58 +0530

    [Shantanu] Added : char array with 10 empty spaces.

---
+++ TicTakToeGame/TicTakToeGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTakToeGame
+{
+    public class TicTakToeGame
+    {
+        public static void TicTakToe()
+        {
+            char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+    }
+}
